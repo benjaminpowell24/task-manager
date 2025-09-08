05145e65f27ce9cc9d330cb478fa86978f7b66d2
Benjamin Powell <benjaminpowell24@gmail.com>
2025-09-08 19:40:06 +0000

    refactor: split components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import TaskList from "./tasks/"
-import { Add } from "iconsax-reactjs"
-import TaskModal from "./tasks/components/TaskModal";
+import TaskList from "./components/Tasks"
+import { Add, Filter } from "iconsax-reactjs"
+import TaskModal from "./components/Tasks/TaskModal";
 import { useTaskContext } from "./context/TaskContext";
   
 
@@ -26,10 +26,16 @@ function App() {
     <div className="min-h-screen p-6 mx-auto max-w-xl">
         {isModalOpen && <TaskModal isModalOpen={isModalOpen} handleModalClose={handleModalClose}/>}
         <h1 className="text-3xl font-bold text-center my-4">Task Manager App</h1>
-        <button className="bg-blue-500 text-white px-2 py-2 rounded flex items-center gap-1 hover:bg-blue-600 cursor-pointer" onClick={handleModalOpen}>
-          <Add />
-          <span>Add Task</span>
+        <div className="flex justify-between">
+          <button className="bg-blue-500 text-white px-2 py-2 rounded flex items-center gap-1 hover:bg-blue-600 cursor-pointer" onClick={handleModalOpen}>
+            <Add />
+            <span>Add Task</span>
           </button>
+          <button className="bg-gray-100 text-gray-500 px-2 py-2 rounded flex items-center gap-1 hover:bg-gray-200 cursor-pointer">
+            <Filter />
+            <span>Filter Tasks</span>
+          </button>
+        </div>
         <TaskList tasks={tasks} />
     </div>
 
