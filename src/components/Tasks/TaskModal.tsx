da718a7850477c0a541d6c84bc5d61cb553609fb
Benjamin Powell <benjaminpowell24@gmail.com>
2025-09-09 00:22:07 +0000

    fix: select dropdown

--- src/components/Tasks/TaskModal.tsx
+++ src/components/Tasks/TaskModal.tsx
@@ -1,15 +1,31 @@
 import { useState, useRef, useEffect } from "react";
 import { useTaskContext } from "../../context/TaskContext";
 import type { TaskModalType, TaskType } from "./types"; 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea"
+
 
 const TaskModal = ({isModalOpen, handleModalClose, mode="create", taskId}: TaskModalType) => {
   const {setTasks} = useTaskContext();
   const [input, setInput] = useState<TaskType>({ id: Date.now(), name: "", description: "", priority: "low" });
   const dialogRef = useRef<HTMLDivElement>(null);
 
+  type priorityType = "low" | "medium" | "high";
+
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setInput((prev) => ({ ...prev, [name]: value }));
+    if (e.target) {
+      const { name, value } = e.target;
+      setInput((prev) => ({ ...prev, [name]: value }));
+    }
   }
 
   const handleCreate = () => {
@@ -37,7 +53,7 @@ const TaskModal = ({isModalOpen, handleModalClose, mode="create", taskId}: TaskM
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (dialogRef.current && !dialogRef.current.contains(event.target as Node)) {
+      if (dialogRef.current && !dialogRef.current.contains(event.target as Node) && !(event.target as HTMLElement).closest("[data-select-dropdown]")) {
         handleModalClose();
       }
     };
@@ -72,23 +88,29 @@ const TaskModal = ({isModalOpen, handleModalClose, mode="create", taskId}: TaskM
      <div className="flex flex-col space-y-4">
      <h1 className="text-xl font-semibold text-center my-4">{mode === "edit" ? "Edit Task" : "Create Task"}</h1>
      <div className="flex flex-col max-w-sm md:max-w-lg px-2 md:mx-auto w-full py-4">
-      <div className="flex flex-col gap-2 lg:grid grid-cols-3">
+      <div className="flex flex-col items-center gap-2 lg:grid grid-cols-3">
       <div className="col-span-2 flex flex-col">
-         <label htmlFor="name" className="font-normal text-gray-500">Task Title</label>
-         <input type="text" name="name" id="name" placeholder="Task Title" className="border p-2 rounded" value={input.name} onChange={handleChange} autoComplete="off"/>
-      </div>
+         <Label htmlFor="name" className="font-normal text-gray-500 mb-2">Task Title</Label>
+         <Input type="text" name="name" id="name" placeholder="Task Title" className="border p-2 rounded" value={input.name} onChange={handleChange} autoComplete="off"/>
+      </div>     
       <div className="flex flex-col">
-         <label htmlFor="priority" className="font-normal text-gray-500">Priority</label>
-         <select name="priority" id="priority" aria-label="Priority" className="border p-2 rounded" value={input.priority} onChange={handleChange}>
-           <option value="low">Low</option>
-           <option value="medium">Medium</option>
-           <option value="high">High</option>
-         </select>
-        </div>
+        <Label htmlFor="priority" className="font-normal text-gray-500 mb-2">Priority</Label>
+        <Select defaultValue="low"   onValueChange={(value) => setInput((prev) => ({ ...prev, priority: value as priorityType }))}
+ value={input.priority}>
+          <SelectTrigger className="hover:cursor-pointer">
+            <SelectValue/>
+         </SelectTrigger>
+         <SelectContent data-select-dropdown>
+           <SelectItem value="low">Low</SelectItem>
+         <SelectItem value="medium">Medium</SelectItem>
+         <SelectItem value="high">High</SelectItem>
+       </SelectContent>
+     </Select>
+      </div>
      </div>
      <div className="flex flex-col mt-2">
-         <label htmlFor="description" className="font-normal text-gray-500">Description</label>
-         <textarea name="description" id="description" placeholder="Task description (Optional)" className="border p-2 rounded field-sizing-content" value={input.description} onChange={handleChange}/>
+         <Label htmlFor="description" className="font-normal text-gray-500 mb-2">Description</Label>
+         <Textarea name="description" id="description" placeholder="Task description (Optional)" className="border p-2 rounded field-sizing-content" value={input.description} onChange={handleChange}/>
       </div>
    
      </div>
